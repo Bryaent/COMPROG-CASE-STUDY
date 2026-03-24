162ec174520197d639bbf21a6f6cb40b4e1c7ea6
Bryaent <bryantrondero@gmail.com>
2026-03-24 21:31:44 +0800

    Implement main class to display product details

---
+++ main.java
@@ -0,0 +1,22 @@
+public class Main {
+    public static void main(String[] args) {
+
+        Product[] items = {
+            new Electronics("Tablet", 10000),
+            new Clothing("Jacket", 5000),
+            new Food("Pizza", 500),
+            new Furniture("Sofa Bed", 30000),
+            new Book("ComProg 2", 500)
+        };
+
+        for (Product p : items) {
+            System.out.println("--------");
+            System.out.println("Type: " + p.getProductType());
+            System.out.println("Name: " + p.getItemLabel());
+            System.out.println("Price: " + p.getBaseCost());
+            System.out.println("Tax: " + p.getTaxPercentage());
+            System.out.println("Net Amount: " + p.getNetAmount());
+        }
+        System.out.println("--------");
+    }
+}
