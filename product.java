bf110769a2bc11776594ffff2cb89250e9686176
Bryaent <bryantrondero@gmail.com>
2026-03-24 21:28:04 +0800

    Implement Product class with attributes and methods

--- product.java
+++ product.java
@@ -1,1 +1,33 @@
+public class Product {
+    private String itemLabel;
+    private int baseCost;
 
+    public Product(String itemLabel, int baseCost){
+        this.itemLabel = itemLabel;
+        this.baseCost = baseCost;
+    }
+
+    public String getItemLabel(){ 
+        return itemLabel; 
+    }
+
+    public int getBaseCost(){ 
+        return baseCost; 
+    }
+
+    public double getTaxRate(){ 
+        return 0; 
+    }
+
+    public double getNetAmount(){ 
+        return getBaseCost() + (getBaseCost() * getTaxRate()); 
+    }
+
+    public String getProductType(){ 
+        return "Product"; 
+    }
+
+    public String getTaxPercentage(){ 
+        return (int)(getTaxRate() * 100) + "%"; 
+    }
+}
