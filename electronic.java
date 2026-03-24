a260c04b95ca29fc4f8f18775a83ab8c0cbfb060
Bryaent <bryantrondero@gmail.com>
2026-03-24 21:29:00 +0800

    Add Electronics class extending Product

--- electronic.java
+++ electronic.java
@@ -1,1 +1,16 @@
+public class Electronics extends Product {
 
+    public Electronics(String itemLabel, int baseCost){
+        super(itemLabel, baseCost);
+    }
+
+    @Override
+    public double getTaxRate(){
+        return 0.12;
+    }
+
+    @Override
+    public String getProductType(){
+        return "Electronics";
+    }
+}
